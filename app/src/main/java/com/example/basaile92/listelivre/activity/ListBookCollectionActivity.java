b5435412fbe4f100cd8782d23b0a1b403056f4d8
merciert <ynot16@hotmail.fr>
2016-11-27 13:42:41 +0100

    Correction of errors on ListBookCollectionActivity class : the fragment is now displaying

--- app/src/main/java/com/example/basaile92/listelivre/activity/ListBookCollectionActivity.java
+++ app/src/main/java/com/example/basaile92/listelivre/activity/ListBookCollectionActivity.java
@@ -1,11 +1,13 @@
 package com.example.basaile92.listelivre.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.basaile92.listelivre.R;
+import com.example.basaile92.listelivre.callback.BookCollectionListFragmentCallBack;
 
-public class ListBookCollectionActivity extends AppCompatActivity {
+public class ListBookCollectionActivity extends FragmentActivity implements BookCollectionListFragmentCallBack{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +16,9 @@ public class ListBookCollectionActivity extends AppCompatActivity {
 
         // Set the title on the top of the screen
         setTitle(R.string.activityCollectionTitle);
+    }
+
+    public void updateDisplayCollectionFragment(int position, View viewLibrary){
+        //todo
     }
 }
