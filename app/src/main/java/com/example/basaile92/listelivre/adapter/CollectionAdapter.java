60db58bade7f9a2292fd06890e93b65e331718ad
merciert <ynot16@hotmail.fr>
2016-11-27 10:51:52 +0100

    Add private classes to CollectionAdapter and implements the method getGroupView

--- app/src/main/java/com/example/basaile92/listelivre/adapter/CollectionAdapter.java
+++ app/src/main/java/com/example/basaile92/listelivre/adapter/CollectionAdapter.java
@@ -2,10 +2,16 @@ package com.example.basaile92.listelivre.adapter;
 
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.basaile92.listelivre.R;
+import com.example.basaile92.listelivre.entity.Collection;
 import com.example.basaile92.listelivre.entity.CollectionList;
 
 /**
@@ -82,11 +88,43 @@ public class CollectionAdapter implements ExpandableListAdapter {
     }
 
 
-    // Return view to obtain a view of book list
+    // Return a view to obtain the collection view
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        //todo
-        return null;
+
+        ParentHolder parentHolder = null;
+
+        Collection collection = (Collection) getGroup(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.collection, null);
+            convertView.setHorizontalScrollBarEnabled(true);
+
+            parentHolder = new ParentHolder();
+            convertView.setTag(parentHolder);
+        } else {
+            parentHolder = (ParentHolder) convertView.getTag();
+        }
+
+        //Set the collection name
+        parentHolder.collectionName = (TextView) convertView.findViewById(R.id.collection_title);
+        parentHolder.collectionName.setText(collection.getName());
+
+        //Set the indicator to see or not books
+        parentHolder.indicator = (ImageView) convertView.findViewById(R.id.image_indicator);
+
+        //Test if the indicator is expanded or not
+        if(isExpanded) {
+            //If the indicator is expanded, the arrow is up
+            parentHolder.indicator.setImageResource(R.drawable.ic_keyboard_arrow_up_black_18dp);
+        } else {
+            //If the indicator is not expanded, the arrow is down
+            parentHolder.indicator.setImageResource(R.drawable.ic_keyboard_arrow_down_black_18dp);
+        }
+
+        return convertView;
+
     }
 
     @Override
@@ -129,5 +167,16 @@ public class CollectionAdapter implements ExpandableListAdapter {
         return 0;
     }
 
+
+    // Class for the list of books
+    private static class ChildHolder {
+        static RecyclerView horizontalListView;
+    }
+
+    // Class for the collection
+    private static class ParentHolder {
+        TextView collectionName;
+        ImageView indicator;
+    }
 
 }
