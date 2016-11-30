fd5b8fcb4accd21efe5583977d241bf4149030df
merciert <ynot16@hotmail.fr>
2016-11-30 17:27:57 +0100

    Beginning of ModifyCollectionActivity

---
+++ app/src/main/java/com/example/basaile92/listelivre/activity/ModifyCollectionActivity.java
@@ -0,0 +1,17 @@
+package com.example.basaile92.listelivre.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.basaile92.listelivre.R;
+
+public class ModifyCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_collection);
+
+        setTitle(R.string.modifyCollectionActivity);
+    }
+}
